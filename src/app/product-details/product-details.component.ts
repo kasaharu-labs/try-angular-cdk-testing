649af49e21dc73e46800a667151cf4a7a7769b81
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-15 17:25:32 +0900

    yarn ng g c product-details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
