b199359e33d469d74edc11ee479df7e86ff851c6
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-15 18:08:11 +0900

    yarn prettier

--- src/app/product-alerts/product-alerts.component.spec.ts
+++ src/app/product-alerts/product-alerts.component.spec.ts
@@ -8,9 +8,8 @@ describe('ProductAlertsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductAlertsComponent ],
-    })
-    .compileComponents();
+      declarations: [ProductAlertsComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
