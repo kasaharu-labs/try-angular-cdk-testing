ab6784ba8fd1b88eb706fde5899a88570c559fab
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-16 23:12:03 +0900

    yarn ng g c header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
