a1dc6ad829d520ad15a098326b4be78922b2e8cd
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-17 09:56:07 +0900

    HeaderComponentHarness を使って AppComponent に HeaderComponent があるかを確認するテストを実装

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeaderComponentHarness } from './header/testing/header.component.harness';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +20,7 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     app = fixture.componentInstance;
   });
 
@@ -23,7 +28,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-basis'`, () => {
-    expect(app.title).toEqual('ng-basis');
+  it(`should have the HeaderComponent`, async () => {
+    const headerComponentHarness = await loader.getHarness(HeaderComponentHarness);
+    const titleText = await headerComponentHarness.getTitleText();
+
+    expect(titleText).toBeTruthy();
   });
 });
