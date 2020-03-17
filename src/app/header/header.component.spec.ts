43a9c775b063dd59525eab1b09d098f4678fb962
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-17 09:55:20 +0900

    HeaderComponentHarness を使って HeaderComponent のテストを実装

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { HeaderComponentHarness } from './testing/header.component.harness';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -20,5 +21,12 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Heading text が kasaharu/try-angular-cdk-testing であること', async () => {
+    const headerComponentHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, HeaderComponentHarness);
+    const titleText = await headerComponentHarness.getTitleText();
+
+    expect(titleText).toBe('kasaharu/try-angular-cdk-testing');
   });
 });
